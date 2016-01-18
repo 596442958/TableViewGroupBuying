f318a6087782cb45762d6816f5e38e3921fcd80b
Miles <596442958@qq.com>
2016-01-18 13:42:19 +0800

    Initial Commit

---
+++ TableView团购数据/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableView团购数据
+//
+//  Created by Miles on 1/18/16.
+//  Copyright © 2016 Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableView团购数据/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableView团购数据
+//
+//  Created by Miles on 1/18/16.
+//  Copyright © 2016 Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
